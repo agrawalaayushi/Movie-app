ead29eeba812cf34282eb8001161c8eb7a2bb009
agrawal.aayushi <agrawal.aayushi13@.com>
2018-08-25 11:27:08 +0530

    loader css improved

--- src/components/Movie/upcoming-movie-list.js
+++ src/components/Movie/upcoming-movie-list.js
@@ -70,6 +70,7 @@ class UpcomingMovieList extends Component {
 
   render() {
     const { upcomingMoviesResponse } = this.props;
+    debugger
     return (
       <div className="movie-list-view">
         { upcomingMoviesResponse ? this.getUpcomingMoviesView() : <Loader/> }
--- src/components/common/loader.js
+++ src/components/common/loader.js
@@ -3,6 +3,8 @@ import  '../../styles/app.scss';
 
 export const Loader = () => {
   return (
-    <div className="loader"></div>
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
   )
 }
